98c77f991eac9d9e9ec9f346340e6da553e5bbea
r-chong <mubblybub@gmail.com>
2023-03-04 02:09:15 -0500

    client now part of root folder

---
+++ client_side/src/useAuth.js
@@ -0,0 +1,11 @@
+import {useState, useEffect} from "react";
+
+export default function useAuth() {
+    const [accessToken, setAccessToken] = useState()
+    const [refreshToken, setRefreshToken] = useState()
+    const [expiresIn, setExpiresin] = useState()
+
+    useEffect(() => {
+        effect
+    },[code])
+}
