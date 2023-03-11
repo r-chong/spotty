c32c8924ab81b1995f95f1b0edfcec398050534a
r-chong <mubblybub@gmail.com>
2023-03-11 02:43:13 -0500

    err invalid_grant, invalid authorization code

--- client/src/useAuth.js
+++ client/src/useAuth.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import axios from 'axios'
 
 export default function useAuth(code) {
@@ -7,17 +7,40 @@ export default function useAuth(code) {
     const [expiresIn, setExpiresIn] = useState()
 
     useEffect(() => {
-        axios.
-        post('http://localhost:3001/login', {
+        axios
+          .post("http://localhost:3001/login", {
             code,
-        })
-        .then(res => {
+          })
+          .then(res => {
             setAccessToken(res.data.accessToken)
             setRefreshToken(res.data.refreshToken)
-            setExpiresIn(res.data.expiresIn)
-            window.history.pushState({},null,'/')
-        })
-    },[code])
+            setExpiresIn(61)
+            window.history.pushState({}, null, "/")
+          })
+          .catch(() => {
+            // if the accesstoken expires, redirect back to / to get new token
+            window.location = "/"
+          })
+      }, [code])
+
+    useEffect(() => {
+        if (!refreshToken || !expiresIn) return
+        const interval = setInterval(() => {
+          axios
+            .post("http://localhost:3001/refresh", {
+              refreshToken,
+            })
+            .then(res => {
+              setAccessToken(res.data.accessToken)
+              setExpiresIn(61)
+            })
+            .catch(() => {
+              window.location = "/"
+            })
+        }, (expiresIn - 60) * 1000)
+        
+        return () => clearInterval(interval)
+    }, [refreshToken, expiresIn])
 
     return accessToken
 }
--- server/server.js
+++ server/server.js
@@ -8,8 +8,29 @@ app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.post('/refresh', (req,res) => {
-    const refreshToken = req.body.refresh
+app.post("/refresh", (req, res) => {
+    const refreshToken = req.body.refreshToken
+    console.log('hi')
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: '75b8ee5250fc4c15b07c36fe24f9b35e',
+        clientSecret: '30b6326f5f454595a1daf3d671ada60e',
+        refreshToken,
+    })
+
+    spotifyApi
+        .refreshAccessToken()
+        .then(data => {
+            console.log(data.body)
+            res.json({
+                accessToken: data.body.accessToken,
+                expiresIn: data.body.expiresIn,
+            })
+        })
+        .catch(err => {
+            console.log(err)
+            res.sendStatus(400)
+        })
 })
 
 app.post('/login', (req,res) => {
@@ -20,17 +41,17 @@ app.post('/login', (req,res) => {
         clientSecret: '30b6326f5f454595a1daf3d671ada60e'
     })
     spotifyApi
-    .authorizationCodeGrant(code)
-    .then(data => {
-        res.json({
-            accessToken: data.body.access_token,
-            refreshToken: data.body.refresh_token,
-            expiresIn: data.body.expires_in,
+        .authorizationCodeGrant(code)
+        .then(data => {
+            res.json({
+                accessToken: data.body.access_token,
+                refreshToken: data.body.refresh_token,
+                expiresIn: data.body.expires_in,
+            })
+        }).catch(err=>{
+            console.log(err)
+            res.sendStatus(400)
         })
-    }).catch(err=>{
-        console.log(err)
-        res.sendStatus(400)
-    })
 })
 
 app.listen(3001)
