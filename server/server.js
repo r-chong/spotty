3d327fa775448a6d452ec1f0bb24ed0bcaac0f75
r-chong <mubblybub@gmail.com>
2023-03-12 00:26:19 -0500

    debugged with chatgpt

--- server/server.js
+++ server/server.js
@@ -4,14 +4,18 @@ const bodyParser = require("body-parser")
 const SpotifyWebApi = require("spotify-web-api-node")
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:3000'
+}))
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.post("/refresh", (req, res) => {
+  console.log("refresh endpoint hit")
   const refreshToken = req.body.refreshToken
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: "http://localhost:3000",
+    redirectUri: "http://localhost:3000/",
     clientId: "75b8ee5250fc4c15b07c36fe24f9b35e",
     clientSecret: "30b6326f5f454595a1daf3d671ada60e",
     refreshToken,
@@ -20,10 +24,11 @@ app.post("/refresh", (req, res) => {
   spotifyApi
     .refreshAccessToken()
     .then(data => {
-      res.json({
-        accessToken: data.body.accessToken,
-        expiresIn: data.body.expiresIn,
-      })
+        res.json({
+            accessToken: data.body.access_token,
+            expiresIn: data.body.expires_in,
+            });
+      console.log("this is refresh check " + data.body.access_token)
     })
     .catch(err => {
       console.log(err)
@@ -32,24 +37,26 @@ app.post("/refresh", (req, res) => {
 })
 
 app.post("/login", (req, res) => {
+  console.log('I have logged in')
   const code = req.body.code
+  console.log("req body code is " + code)
   const spotifyApi = new SpotifyWebApi({
-    redirectUri: "http://localhost:3000",
+    redirectUri: "http://localhost:3000/",
     clientId: "75b8ee5250fc4c15b07c36fe24f9b35e",
     clientSecret: "30b6326f5f454595a1daf3d671ada60e",
   })
 
-  spotifyApi
-    .authorizationCodeGrant(code)
+  spotifyApi.authorizationCodeGrant(code)
     .then(data => {
       res.json({
         accessToken: data.body.access_token,
         refreshToken: data.body.refresh_token,
-        expiresIn: data.body.expires_in,
+        expiresIn: data.body.expires_in
       })
+      console.log("this is login check " + data.body.access_token)
     })
     .catch(err => {
-        console.log(err)
+      console.log(err)
       res.sendStatus(400)
     })
 })
